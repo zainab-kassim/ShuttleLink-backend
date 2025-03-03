6211f7c726f70211b740e119353c31307df99f20
Zainab Kassim <kassimzainab11@gmail.com>
2025-03-03 21:46:52 +0100

    no env

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv'
-import express from 'express';
+// import dotenv from 'dotenv'
+import express from 'express'
 import smsRoutes from './routes/sms.routes.js'
 import userRoutes from './routes/user.routes.js'
 import rideRoutes from './routes/ride.routes.js'
@@ -11,10 +11,10 @@ import passport from 'passport'
 
 
 
-// Load environment variables if not in production
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config();
-}
+// // Load environment variables if not in production
+// if (process.env.NODE_ENV !== 'production') {
+//     dotenv.config();
+// }
 
 
 
